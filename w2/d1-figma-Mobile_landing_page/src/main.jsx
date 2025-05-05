449c0eef4695f024c2f8889f66343bea2ed4fd11
Abhey Singh <abheys172@gmail.com>
2025-05-05 22:28:54 +0530

    kk

--- w2/d1-figma-Mobile_landing_page/src/main.jsx
+++ w2/d1-figma-Mobile_landing_page/src/main.jsx
@@ -2,15 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'remixicon/fonts/remixicon.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-    
+    <BrowserRouter>
       <App />
-    
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
